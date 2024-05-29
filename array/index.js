ad565ddcf5cc9fd6013f07616d6429a5ec0f30e3
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2024-05-30 01:43:25 +0600

    array split

---
+++ array/index.js
@@ -0,0 +1,42 @@
+/*
+// Use of at() Array
+
+// const fruits= ['banana','orange','mango','apple'];
+const fruits= ['0','1','2','3'];
+let fruit= fruits[2];
+
+// console.log(fruit);
+
+//use of concat() array
+
+const arr1=['namdm','asif'];
+const arr2= ['rubel','hassan'];
+
+let include= arr1.concat(arr2);
+
+
+const arr1=['0','1'];
+const arr2= ['2','3'];
+const arr3= ['4','5']
+
+let include= arr1.concat(arr2,arr3);
+
+console.log(include);
+
+
+
+//use of array().form 
+
+ 
+let Title="i love my self";
+let TitleArray=Array.from(Title);
+
+console.log(TitleArray);
+
+
+//use of split('')/split(' ')
+
+let string = "i fell in love with java script language";
+console.log(string.split(""))
+*/
+
